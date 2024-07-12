ca862ee6cc0a24a99f2182b5c5d52fe2f40934d5
vinodsarwade <vinodsarwade7@gmail.com>
2024-07-12 23:29:52 +0530

    vinod

---
+++ mathemetical_operations/file1.py
@@ -0,0 +1,18 @@
+import numpy as np
+# arr1 = np.array([30,40,50,60])
+# arr2 = np.array([20,30,40,50])
+# print(arr1 + arr2)
+#print(np.add(arr1, arr2))
+
+
+arr1 = np.array([[30,40],[50,60]])
+arr2 = np.array([[20,30],[40,50]])
+print(arr1 + arr2)
+# print(np.add(arr1, arr2)) # this is same as above
+
+# print(arr1 - arr2)
+print(np.subtract(arr1, arr2))
+print(np.multiply(arr1, arr2))
+print(np.divide(arr1, arr2))
+
+
---
+++ mathemetical_operations/file2.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr1 = np.array([3,4,5,6])
+arr2 = np .array([2])
+print(np.power(arr1, arr2))
+
+
+arr = np.array([64,81,25])
+print(np.sqrt(arr))
